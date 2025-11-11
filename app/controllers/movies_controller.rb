3400dbc541f054c952341e149a71f1961a881b30
Ananya Shah <acs9931@nyu.edu>
2025-11-11 17:37:31 +0000

    Filter by rating + sort by title/release_date; fix controller name

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,20 +1,68 @@
-def index
-  # All possible ratings
-  @all_ratings = Movie.all_ratings
+class MoviesController < ApplicationController
+  before_action :set_movie, only: %i[show edit update destroy]
 
-  # Ratings selected (default = all)
-  if params[:ratings].present?
-    @ratings_to_show = params[:ratings].keys
-  else
-    @ratings_to_show = @all_ratings
+  # GET /movies
+  def index
+    # all possible ratings for the checkboxes
+    @all_ratings = Movie.all_ratings
+
+    # which ratings should be shown (default = all)
+    @ratings_to_show =
+      if params[:ratings].present?
+        params[:ratings].keys
+      else
+        @all_ratings
+      end
+
+    # which column to sort by (optional)
+    @sort_by = params[:sort_by].presence
+
+    # base scope filtered by ratings
+    @movies = Movie.with_ratings(@ratings_to_show)
+
+    # apply sort if valid
+    if %w[title release_date].include?(@sort_by)
+      @movies = @movies.order(@sort_by => :asc)
+    end
   end
 
-  # Sorting selected (default = nil)
-  @sort_by = params[:sort_by]
+  def show; end
 
-  # Query the movies
-  @movies = Movie.with_ratings(@ratings_to_show)
+  def new
+    @movie = Movie.new
+  end
 
-  # Apply sorting if provided
-  @movies = @movies.order(@sort_by) if @sort_by.present?
+  def edit; end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to @movie, notice: "Movie was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @movie.update(movie_params)
+      redirect_to @movie, notice: "Movie was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @movie.destroy!
+    redirect_to movies_path, status: :see_other, notice: "Movie was successfully destroyed."
+  end
+
+  private
+
+  def set_movie
+    @movie = Movie.find(params[:id])
+  end
+
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
+  end
 end
