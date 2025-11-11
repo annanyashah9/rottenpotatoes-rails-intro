c01e129d6366010837b02222140e396e41eabdd3
Ananya Shah <acs9931@nyu.edu>
2025-11-11 17:30:26 +0000

    Add sorting dropdown and controller sorting logic

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,73 +1,20 @@
-class MoviesController < ApplicationController
-  before_action :set_movie, only: %i[ show edit update destroy ]
+def index
+  # All possible ratings
+  @all_ratings = Movie.all_ratings
 
-  # GET /movies
-  def index
-    # all possible ratings (from the model)
-    @all_ratings = Movie.all_ratings
-
-    # which ratings should be shown (default: all checked / first visit or none selected)
-    @ratings_to_show = params[:ratings]&.keys || @all_ratings
-
-    # fetch movies filtered by selected ratings
-    @movies = Movie.with_ratings(@ratings_to_show)
+  # Ratings selected (default = all)
+  if params[:ratings].present?
+    @ratings_to_show = params[:ratings].keys
+  else
+    @ratings_to_show = @all_ratings
   end
 
-  # GET /movies/1
-  def show; end
+  # Sorting selected (default = nil)
+  @sort_by = params[:sort_by]
 
-  # GET /movies/new
-  def new
-    @movie = Movie.new
-  end
+  # Query the movies
+  @movies = Movie.with_ratings(@ratings_to_show)
 
-  # GET /movies/1/edit
-  def edit; end
-
-  # POST /movies
-  def create
-    @movie = Movie.new(movie_params)
-
-    respond_to do |format|
-      if @movie.save
-        format.html { redirect_to @movie, notice: "Movie was successfully created." }
-        format.json { render :show, status: :created, location: @movie }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @movie.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /movies/1
-  def update
-    respond_to do |format|
-      if @movie.update(movie_params)
-        format.html { redirect_to @movie, notice: "Movie was successfully updated." }
-        format.json { render :show, status: :ok, location: @movie }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @movie.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /movies/1
-  def destroy
-    @movie.destroy!
-    respond_to do |format|
-      format.html { redirect_to movies_path, status: :see_other, notice: "Movie was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-
-    def set_movie
-      @movie = Movie.find(params[:id])
-    end
-
-    def movie_params
-      params.require(:movie).permit(:title, :rating, :description, :release_date)
-    end
+  # Apply sorting if provided
+  @movies = @movies.order(@sort_by) if @sort_by.present?
 end
